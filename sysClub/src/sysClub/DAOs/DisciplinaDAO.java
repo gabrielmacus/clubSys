954d7f8044b66a411c46be0437e36d88bba4af08
Pablofns <pablofns@gmail.com>
2016-02-17 10:24:43 -0300

    disciplinaDAO

---
+++ sysClub/src/sysClub/DAOs/DisciplinaDAO.java
@@ -0,0 +1,46 @@
+package sysClub.DAOs;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import sysClub.PersistenceManager;
+import sysClub.entidades.Disciplina;
+
+public class DisciplinaDAO {
+	
+	public void addDisciplina(Disciplina d)
+	{
+		EntityManager em = PersistenceManager.getInstance().getEntityManager();
+		em.getTransaction().begin();
+		em.persist(d);
+		em.getTransaction().commit();
+	}
+	
+	public void deleteDisciplina(Long id)
+	{
+		EntityManager em = PersistenceManager.getInstance().getEntityManager();
+		em.getTransaction().begin();
+		Disciplina disc = em.find(Disciplina.class, id);
+		if (disc!=null)
+		{
+			
+			EntityTransaction txn= em.getTransaction();
+			txn.begin();
+			try{
+				em.remove(disc);
+				txn.commit();
+			}catch(Exception ex)
+			{
+				ex.printStackTrace();
+				if(txn.isActive() )
+					txn.rollback();
+				throw ex;
+				
+			}
+					
+		}
+		em.close();
+	}
+	
+
+}
